6cfa0d4b3c20d6ab5d7c7bd731e6ae051fdc7508
joshuaalawode <joshalawode@gmail.com>
2018-08-21 17:14:10 -0400

    PostgreSQL setup for the users service

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 # Instantiate the app
 app = Flask(__name__)
@@ -8,6 +9,22 @@ app = Flask(__name__)
 app_settings = os.getenv("APP_SETTINGS")
 app.config.from_object(app_settings)
 
+# Instantiate the database
+db = SQLAlchemy(app)
+
+# Database Model
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+# Routes
 @app.route('/users/ping', methods=['GET'])
 def ping_pong():
     return jsonify({
