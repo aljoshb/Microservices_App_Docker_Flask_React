60bd55fe16570562ee3a29d56a982c91d344de8e
joshuaalawode <joshalawode@gmail.com>
2018-08-28 00:14:36 -0400

    First React tests pass

--- services/client/src/components/__tests__/UsersList.test.jsx
+++ services/client/src/components/__tests__/UsersList.test.jsx
@@ -22,5 +22,5 @@ test('UsersList renders properly', () => {
     const wrapper = shallow(<UsersList users={users}/>);
     const element = wrapper.find('h4');
     expect(element.length).toBe(2);
-    expect(element.get(0).props.children).toBe('josh')
-})
+    expect(element.get(0).props.children).toBe('josh');
+});
