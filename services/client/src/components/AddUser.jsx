b6ea62f5790f763fcc48013895760a175694b6d5
joshuaalawode <joshalawode@gmail.com>
2018-08-28 04:24:06 -0400

    Added React forms

--- services/client/src/components/AddUser.jsx
+++ services/client/src/components/AddUser.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 
 const AddUser = (props) => {
     return  (
-        <form>
+        <form onSubmit={(event) => props.addUser(event)}>
             <div className="field">
                 <input
                     name="username"
                     className="input is-large"
-                    type="test"
+                    type="text"
                     placeholder="Enter a username" required
+                    value={props.username}
+                    onChange={props.handleChange}
                 />
             </div>
             <div className="field">
@@ -17,6 +19,8 @@ const AddUser = (props) => {
                     className="input is-large"
                     type="email"
                     placeholder="Enter an email address" required
+                    value={props.email}
+                    onChange={props.handleChange}
                 />
             </div>
             <input
--- services/client/src/index.js
+++ services/client/src/index.js
@@ -9,14 +9,41 @@ class App extends Component {
     constructor() {
         super();
         this.state = {
-            users: []
+            users: [],
+            username: '',
+            email: '',
         };
+        this.addUser = this.addUser.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
 
     getUsers() {
         axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
         .then((res) => { this.setState({ users: res.data.data.users }); })
         .catch((err) => { console.log(err); }); 
+    }
+
+    addUser(event) {
+        event.preventDefault();
+        console.log('sanity check!');
+        console.log(this.state);
+    
+        const data = {
+            username: this.state.username,
+            email: this.state.email
+        };
+        axios.post(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`, data)
+        .then((res) => { 
+            this.getUsers();
+            this.setState({ username: '', email: ''});
+        })
+        .catch((err) => { console.log(err); });
+    }
+
+    handleChange(event) {
+        const obj = {};
+        obj[event.target.name] = event.target.value;
+        this.setState(obj);
     }
 
     componentDidMount() {
@@ -32,7 +59,12 @@ class App extends Component {
                         <br/>
                         <h1 className="title is-1 is-1">All Users</h1>
                         <hr/><br/>
-                        <AddUser/>
+                        <AddUser
+                            username={this.state.username}
+                            email={this.state.email}
+                            addUser={this.addUser}
+                            handleChange={this.handleChange}
+                        />
                         <br/><br/>
                         <UsersList users={this.state.users} />
                         </div>
