a2b0e09b685945c9e8485ca1c3208b40d121707f
joshuaalawode <joshalawode@gmail.com>
2018-09-03 01:26:20 -0500

    Fixed the bugs

--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import axios from 'axios';
 
 import UsersList from './components/UsersList';
@@ -36,7 +37,10 @@ class App extends Component {
             this.getUsers();
             this.setState({ username: '', email: ''});
         })
-        .catch((err) => { console.log(err); });
+        .catch((err) => {
+            console.log('an error while trying to add user');
+            console.log(err); 
+        });
     }
 
     handleChange(event) {
@@ -55,19 +59,24 @@ class App extends Component {
                 <div className="container">
                     <div className="columns">
                         <div className="column is-half">
-                        <br/>
-                        <h1 className="title is-1 is-1">All Users</h1>
-                        <hr/><br/>
-                        <AddUser
-                            username={this.state.username}
-                            email={this.state.email}
-                            addUser={this.addUser}
-                            handleChange={this.handleChange}
-                        />
-                        <br/><br/>
-                        <UsersList users={this.state.users} />
-                        <br/>
-                        <About />
+                            <br/>
+                            <Switch>
+                                <Route exact path='/' render={() => (
+                                    <div>
+                                        <h1 className="title is-1 is-1">All Users</h1>
+                                        <hr/><br/>
+                                        <AddUser
+                                            username={this.state.username}
+                                            email={this.state.email}
+                                            addUser={this.addUser}
+                                            handleChange={this.handleChange}
+                                        />
+                                        <br/><br/>
+                                        <UsersList users={this.state.users} />
+                                    </div>
+                                )} />
+                                <Route exact path='/about' component={About} />
+                            </Switch>
                         </div>
                     </div>
                 </div>
--- services/client/src/index.js
+++ services/client/src/index.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
+
 
 ReactDOM.render((
     <Router>
