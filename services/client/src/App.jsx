c170dbd52fada08b462b109f3145f4a00b4cd6e8
joshuaalawode <joshalawode@gmail.com>
2018-09-08 01:29:49 -0400

    Client side login and logout flow set up

--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -7,6 +7,7 @@ import AddUser from './components/AddUser';
 import About from './components/About';
 import NavBar from './components/NavBar';
 import Form from './components/Form';
+import Logout from './components/Logout';
 
 
 class App extends Component {
@@ -29,6 +30,7 @@ class App extends Component {
         this.handleChange = this.handleChange.bind(this);
         this.handleUserFormSubmit = this.handleUserFormSubmit.bind(this);
         this.handleFormChange = this.handleFormChange.bind(this);
+        this.logoutUser = this.logoutUser.bind(this);
     }
 
     getUsers() {
@@ -95,6 +97,11 @@ class App extends Component {
         });
     }
 
+    logoutUser() {
+        window.localStorage.clear();
+        this.setState({ isAuthenticated: false });
+    }
+
     componentDidMount() {
         this.getUsers();
     }
@@ -139,6 +146,12 @@ class App extends Component {
                                             formData={this.state.formData}
                                             handleUserFormSubmit={this.handleUserFormSubmit}
                                             handleFormChange={this.handleFormChange}
+                                            isAuthenticated={this.state.isAuthenticated}
+                                        />
+                                    )} />
+                                    <Route exact path='/logout' render={() => (
+                                        <Logout
+                                            logoutUser={this.logoutUser}
                                             isAuthenticated={this.state.isAuthenticated}
                                         />
                                     )} />
