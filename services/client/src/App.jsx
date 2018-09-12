bdc384b8d14c6118b714b706ba0a8a931bc08661
joshuaalawode <joshalawode@gmail.com>
2018-09-11 22:30:06 -0400

    Basic client user status route setup

--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -8,6 +8,7 @@ import About from './components/About';
 import NavBar from './components/NavBar';
 import Form from './components/Form';
 import Logout from './components/Logout';
+import UserStatus from './components/UserStatus';
 
 
 class App extends Component {
@@ -155,6 +156,7 @@ class App extends Component {
                                             isAuthenticated={this.state.isAuthenticated}
                                         />
                                     )} />
+                                    <Route exact path='/status' component={UserStatus} />
                                 </Switch>
                             </div>
                         </div>
