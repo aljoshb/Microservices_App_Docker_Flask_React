de61a1c748613902c3a39b25dbfc2b4c8576d3c6
joshuaalawode <joshalawode@gmail.com>
2018-09-22 23:37:55 -0400

    Added some end-to-end tests for the login/logout route

---
+++ cypress/integration/login.test.js
@@ -0,0 +1,64 @@
+const randomstring = require('randomstring');
+
+const username = randomstring.generate();
+const email = `${username}@test.com`;
+
+describe('Login', () => {
+    it('should display the sign in form', () => {
+        cy
+            .visit('/login')
+            .get('h1').contains('Login')
+            .get('form');
+    });
+    it('should allow a user to sign in', () => {
+        // Register a user
+        cy
+            .visit('/register')
+            .get('input[name="username"]').type(username)
+            .get('input[name="email"]').type(email)
+            .get('input[name="password"]').type('test')
+            .get('input[name="submit"]').click();
+
+        // Log a user out
+        cy.get('.navbar-burger').click();
+        cy.contains('Log Out').click();
+
+        // Log a user in
+        cy
+            .get('a').contains('Log In').click()
+            .get('input[name="email"]').type(email)
+            .get('input[name="password"]').type('test')
+            .get('input[name="submit"]').click()
+            .wait(100);
+
+        // Assert user is redirected to '/'
+        // Assert '/' is displayed properly
+        cy.contains('All Users');
+        cy
+            .get('table')
+            .find('tbody > tr').last()
+            .find('td').contains(username);
+        cy.get('.navbar-burger').click();
+        cy.get('.navbar-menu').within( () => {
+            cy
+                .get('.navbar-item').contains('User status')
+                .get('.navbar-item').contains('Log Out')
+                .get('.navbar-item').contains('Log In').should('not.be.visible')
+                .get('.navbar-item').contains('Register').should('not.be.visible');
+        });
+
+        // Log a user out
+        cy
+            .get('a').contains('Log Out').click();
+
+        // Asser '/logout' is displayed properly
+        cy.get('p').contains('You are now logged out');
+        cy.get('.navbar-menu').within(() => {
+            cy
+            .get('.navbar-item').contains('User status').should('not.be.visible')
+            .get('.navbar-item').contains('Log Out').should('not.be.visible')
+            .get('.navbar-item').contains('Log In')
+            .get('.navbar-item').contains('Register');
+        });
+    });
+});
