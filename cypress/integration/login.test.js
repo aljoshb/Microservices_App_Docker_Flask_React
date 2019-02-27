2c7e96d13fd31b1a6e52955dae9e81428f576b0e
joshuaalawode <joshalawode@gmail.com>
2019-02-27 00:19:22 -0500

    one step closer to fixing the end to end test failures

--- cypress/integration/login.test.js
+++ cypress/integration/login.test.js
@@ -21,45 +21,45 @@ describe('Login', () => {
 
         // Log a user out
         cy.get('.navbar-burger').click();
-        // cy.contains('Log Out').click();
+        cy.contains('Log Out').click();
 
         // Log a user in
-        // cy
-        //     .get('a').contains('Log In').click()
-        //     .get('input[name="email"]').type(email)
-        //     .get('input[name="password"]').type('test')
-        //     .get('input[type="submit"]').click()
-        //     .wait(100);
+        cy
+            .get('a').contains('Log In').click()
+            .get('input[name="email"]').type(email)
+            .get('input[name="password"]').type('test')
+            .get('input[type="submit"]').click()
+            .wait(100);
         
-        // // Assert user is redirected to '/'
-        // // Assert '/' is displayed properly
-        // cy.get('.navbar-burger').click();
-        // cy.contains('All Users');
-        // cy
-        //     .get('table')
-        //     .find('tbody > tr').last()
-        //     .find('td').contains(username);
-        // cy.get('.navbar-burger').click();
-        // cy.get('.navbar-menu').within( () => {
-        //     cy
-        //         .get('.navbar-item').contains('User Status')
-        //         .get('.navbar-item').contains('Log Out')
-        //         .get('.navbar-item').contains('Register').should('not.be.visible')
-        //         .get('.navbar-item').contains('Log In').should('not.be.visible');
-        // });
+        // Assert user is redirected to '/'
+        // Assert '/' is displayed properly
+        cy.get('.navbar-burger').click();
+        cy.contains('All Users');
+        cy
+            .get('table')
+            .find('tbody > tr').last()
+            .find('td').contains(username);
+        cy.get('.navbar-burger').click();
+        cy.get('.navbar-menu').within( () => {
+            cy
+                .get('.navbar-item').contains('User Status')
+                .get('.navbar-item').contains('Log Out')
+                .get('.navbar-item').contains('Register').should('not.be.visible')
+                .get('.navbar-item').contains('Log In').should('not.be.visible');
+        });
 
-        // // Log a user out
-        // cy
-        //     .get('a').contains('Log Out').click();
+        // Log a user out
+        cy
+            .get('a').contains('Log Out').click();
 
-        // // Asser '/logout' is displayed properly
-        // cy.get('p').contains('You are now logged out');
-        // cy.get('.navbar-menu').within(() => {
-        //     cy
-        //     .get('.navbar-item').contains('User Status').should('not.be.visible')
-        //     .get('.navbar-item').contains('Log Out').should('not.be.visible')
-        //     .get('.navbar-item').contains('Log In')
-        //     .get('.navbar-item').contains('Register');
-        // });
+        // Asser '/logout' is displayed properly
+        cy.get('p').contains('You are now logged out');
+        cy.get('.navbar-menu').within(() => {
+            cy
+            .get('.navbar-item').contains('User Status').should('not.be.visible')
+            .get('.navbar-item').contains('Log Out').should('not.be.visible')
+            .get('.navbar-item').contains('Log In')
+            .get('.navbar-item').contains('Register');
+        });
     });
 });
--- cypress/integration/register.test.js
+++ cypress/integration/register.test.js
@@ -22,15 +22,15 @@ describe('Register', () => {
         // Assert user is redirected to '/'
         // Assert '/' is displayed properly
         cy.get('.navbar-burger').click();
-        // cy.contains('All Users');
-        // cy.contains(username);
-        // cy.get('.navbar-burger').click();
-        // cy.get('.navbar-menu').within(() => {
-        //     cy
-        //         .get('.navbar-item').contains('User Status')
-        //         .get('.navbar-item').contains('Log Out')
-        //         .get('.navbar-item').contains('Log In').should('not.be.visible')
-        //         .get('.navbar-item').contains('Register').should('not.be.visible');
-        // });
+        cy.contains('All Users');
+        cy.contains(username);
+        cy.get('.navbar-burger').click();
+        cy.get('.navbar-menu').within(() => {
+            cy
+                .get('.navbar-item').contains('User Status')
+                .get('.navbar-item').contains('Log Out')
+                .get('.navbar-item').contains('Log In').should('not.be.visible')
+                .get('.navbar-item').contains('Register').should('not.be.visible');
+        });
     });
 });
